0be8efd9752fff2ca4a3dbddc7f7a507f6de9d49
Lewis (Linh) Pham <linh.pham.tpv@one-line.com>
2025-10-10 22:12:47 +0700

    cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,18 @@
 'use client';
 import Link from 'next/link';
 import { useCartStore } from '@/store/cartStore';
+import { useEffect } from 'react';
 
 export default function Navbar() {
   const count = useCartStore((s) =>
     s.items.reduce((sum, i) => sum + i.quantity, 0)
   );
+
+  const fetchCarts = useCartStore(s => s.fetchCarts);
+
+  useEffect(() => {
+    fetchCarts()
+  }, [fetchCarts])
 
   return (
     <nav className="bg-gray-800 text-white px-8 py-4 flex justify-between">
--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -7,30 +7,120 @@ export interface CartItem {
   quantity: number;
 }
 
+interface CartItemResponse {
+  id: number;
+  productId: number;
+  productName: string;
+  unitPrice: number;
+  quantity: number;
+  lineAmount: number;
+}
+
 interface CartState {
+  loading: boolean;
+  error?: string;
   items: CartItem[];
+  fetchCarts: () => void;
   addItem: (item: CartItem) => void;
   removeItem: (id: number) => void;
   clearCart: () => void;
 }
 
-export const useCartStore = create<CartState>((set) => ({
-  items: [],
-  addItem: (item) =>
-    set((state) => {
-      const existing = state.items.find((i) => i.id === item.id);
-      if (existing) {
-        return {
-          items: state.items.map((i) =>
-            i.id === item.id
-              ? { ...i, quantity: i.quantity + item.quantity }
-              : i
-          ),
-        };
+const fetchBase = async (endPoint: string, item: unknown, success: (data: CartItemResponse | CartItemResponse[]) => void, failure: (error: Error) => void) => {
+  return fetch('http://localhost:3005' + endPoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(item)
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
       }
-      return { items: [...state.items, item] };
-    }),
-  removeItem: (id) =>
-    set((state) => ({ items: state.items.filter((i) => i.id !== id) })),
-  clearCart: () => set({ items: [] }),
+      return res.json()
+    })
+    .then((data) => {
+      success(data)
+    })
+    .catch(error => {
+      failure(error)
+    })
+}
+
+export const useCartStore = create<CartState>((set) => ({
+  loading: true,
+  error: undefined,
+  items: [],
+  fetchCarts: () => {
+    fetchBase(
+      '/get-items',
+      {},
+      (data) => {
+        const items = (data as CartItemResponse[]).map(i => ({
+          id: i.productId,
+          name: i.productName,
+          sellingPrice: i.unitPrice,
+          quantity: i.quantity,
+        }))
+        set({ items: items as CartItem[], loading: false })
+      },
+      (error) => {
+        set({ error: error.message, loading: false })
+      }
+    )
+  },
+  addItem: (item) => {
+    set({ loading: true })
+    fetchBase(
+      '/add-item',
+      {
+        productId: item.id,
+        unitPrice: item.sellingPrice,
+        quantity: item.quantity
+      },
+      () => {
+        set((state) => {
+          const existing = state.items.find((i) => i.id === item.id);
+          if (existing) {
+            return {
+              items: state.items.map((i) =>
+                i.id === item.id
+                  ? {
+                    ...i,
+                    quantity: i.quantity + item.quantity
+                  }
+                  : i
+              ),
+              loading: false
+            };
+          }
+          return { items: [...state.items, item], loading: false };
+        })
+      },
+      (error) => {
+        set({ error: error.message, loading: false })
+      });
+  },
+  removeItem: (id) => {
+    set({ loading: true })
+    fetchBase(
+      '/remove-item',
+      { productId: id },
+      () => {
+        set((state) => ({ items: state.items.filter((i) => i.id !== id), loading: false }))
+      },
+      (error) => {
+        set({ error: error.message, loading: false })
+      }
+    )
+  },
+  clearCart: () => {
+    set({ loading: true })
+    fetchBase(
+      '/clear',
+      {},
+      () => set({ items: [], loading: false }),
+      (error) => set({ error: error.message, loading: false }))
+  },
 }));
